7f47fdf4c6e2949da5267fb915e1fec7bc854b94
“liujk” <1252327158@qq.com>
2019-05-05 17:46:43 +0800

    初始化

---
+++ src/main/java/com/leet/algo/ques/No16.java
@@ -0,0 +1,15 @@
+package com.leet.algo.ques;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author jkliu
+ * @description
+ * @create 2019-05-05 17:33
+ **/
+@Component
+public class No16 {
+    public int threeSumClosest(int[] nums, int target) {
+        return 2;
+    }
+}
---
+++ src/test/java/com/leet/algo/ques/No16Test.java
@@ -0,0 +1,35 @@
+package com.leet.algo.ques; 
+
+import com.leet.algo.AlgoApplication;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.Before; 
+import org.junit.After;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/** 
+* No16 Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>May 5, 2019</pre> 
+* @version 1.0 
+*/
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = AlgoApplication.class)
+public class No16Test {
+    @Autowired
+    No16 no16;
+
+    @Test
+    public void testThreeSumClosest() throws Exception {
+
+        int[] nums = {-3, 2, 0, 9, 5, 1};
+        int res = no16.threeSumClosest(nums, 4);
+        Assert.assertEquals(2, res);
+    }
+
+
+} 
