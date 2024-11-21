169cdf6561de948a1514bba9556adb0d6bcbbb13
“liujk” <1252327158@qq.com>
2024-11-21 09:35:02 +0800

    feat:update

---
+++ src/main/java/com/leet/leetCode101/algorithm/greedy/No435.java
@@ -0,0 +1,9 @@
+package com.leet.leetCode101.algorithm.greedy;
+
+/**
+ * @description
+ * @author jkliu
+ * @create 2024-11-19 11:20
+ **/
+public class No435 {
+}
--- src/main/java/com/leet/leetCode101/algorithm/greedy/No455.java
+++ src/main/java/com/leet/leetCode101/algorithm/greedy/No455.java
@@ -1,9 +1,25 @@
 package com.leet.leetCode101.algorithm.greedy;
 
+import java.util.Arrays;
+
 /**
  * @description
  * @author jkliu
  * @create 2024-11-14 14:16
  **/
 public class No455 {
+    public int findContentChildren(int[] g, int[] s) {
+        if (g.length == 0 || s.length == 0) {
+            return 0;
+        }
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = 0;
+        for (int x : s) {
+            if (i < g.length && g[i] <= x) {
+                i++;
+            }
+        }
+        return i;
+    }
 }
